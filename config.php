5676ef28ca7b066d42624461e36af517acacaf8e
Brando <brando@Brandos-MacBook-Pro.local>
2018-09-27 03:10:08 -0400

    testing cloud implementation

--- config.php
+++ config.php
@@ -5,11 +5,19 @@
  *
  */
 
-$host       = "localhost";
-$username   = "root";
-$password   = "root";
-$dbname     = "test";
+//$host       = "localhost";
+//$username   = "root";
+//$password   = "root";
+//$dbname     = "test";
+//$dsn        = "mysql:host=$host;dbname=$dbname";
+//$options    = array(
+//                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+//              );
+$host       = "den1.mysql2.gear.host";
+$username   = "sad1";
+$password   = "Wj7EuZU-2~6G";
+$dbname     = "sad1";
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-              );
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+);
